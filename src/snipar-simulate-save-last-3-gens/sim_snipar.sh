5a0564dbbb1d0fe3f4f8c12cf04f9520beb7bf27
imahdimir <imahdimir@gmail.com>
2023-11-09 20:12:34 +0330

    n_last, uibd

--- src/snipar-simulate-save-last-3-gens/sim_snipar.sh
+++ src/snipar-simulate-save-last-3-gens/sim_snipar.sh
@@ -11,3 +11,5 @@ pgs.py direct_v1_obs --pgs direct_v1_obs.pgs.txt --phenofile phenotype.txt
 # % ture direct effect 
 pgs.py direct_obs --bed chr_@_combined --weights causal_effects.txt --beta_col direct --pedigree pedigree.txt
 pgs.py direct_obs --pgs direct_obs.pgs.txt --phenofile phenotype.txt
+
+
