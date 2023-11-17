854207da054025260bf9dedf339cda79f241b2f1
imahdimir <imahdimir@gmail.com>
2023-11-18 01:20:07 +0330

    removal of parental gen from pgs file and redo the analysis using Snipar

--- src/snipar-simulate-save-last-3-gens/three_gen.sh
+++ src/snipar-simulate-save-last-3-gens/three_gen.sh
@@ -4,8 +4,12 @@ plink --bfile chr_1_off_and_par --bmerge chr_1_gpar --out chr_1_combined
 
 # % true direct effect
 pgs.py direct_obs --bed chr_@_combined --weights causal_effects.txt --beta_col direct --pedigree pedigree.txt --grandpar
-pgs.py direct_obs --pgs direct_obs.pgs.txt --phenofile phenotype.txt --gen_models 1-3
 
 # % ture direct effect + noise(v1)
 pgs.py direct_v1_obs --bed chr_@_combined --weights causal_effects.txt --beta_col direct_v1 --pedigree pedigree.txt --grandpar
-pgs.py direct_v1_obs --pgs direct_v1_obs.pgs.txt --phenofile phenotype.txt --gen_models 1-3
+
+# % I removed the parent gen from pgs file and kept only the last(offspring) gen using R then I used new pgs for the following
+pgs.py direct_obs --pgs direct_obs_last_gen.pgs.txt --phenofile phenotype.txt --gen_models 1-3
+pgs.py direct_v1_obs --pgs direct_v1_obs_last_gen.pgs.txt --phenofile phenotype.txt --gen_models 1-3
+
+
