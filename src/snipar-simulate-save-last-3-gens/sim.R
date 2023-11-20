007c9e4c83c03eb4b5e3220948639e4a90d4e858
imahdimir <imahdimir@gmail.com>
2023-11-20 14:36:16 +0330

    v10

--- src/snipar-simulate-save-last-3-gens/sim.R
+++ src/snipar-simulate-save-last-3-gens/sim.R
@@ -1,4 +1,4 @@
-wd = "/Users/mahdi/Dropbox/1-Git/snipar-simulate-save-last-3-gens/src/snipar-simulate-save-last-3-gens/sim"
+wd = "/Users/mahdi/Dropbox/1-Git/snipar-simulate-save-last-3-gens/src/snipar-simulate-save-last-3-gens/sim_new"
 setwd(wd)
 
 d = read.table("direct_obs.pgs.txt", header = T)
@@ -16,3 +16,16 @@ d = d[sapply(d$FID, function(x) strsplit(x, "_")[[1]][1]) == "23",]
 write.table(d, 'direct_v1_obs_last_gen.pgs.txt', quote = FALSE, row.names = FALSE)
 
 cor(d[, -c(1:4)])
+
+
+
+d = read.table("direct_v10_obs.pgs.txt", header = T)
+
+d = d[sapply(d$FID, function(x) strsplit(x, "_")[[1]][1]) == "23",]
+
+write.table(d, 'direct_v10_obs_last_gen.pgs.txt', quote = FALSE, row.names = FALSE)
+
+cor(d[, -c(1:4)])
+
+
+
