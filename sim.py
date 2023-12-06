1acc06d5f6ec14e6dc2339fdd3afc42630c85427
imahdimir <imahdimir@gmail.com>
2023-12-06 17:55:40 +0330

    update

---
+++ sim.py
@@ -0,0 +1,40 @@
+"""
+
+    """
+
+from simulation_code.simulate import main
+
+class WithIndirArgs :
+    n_causal = 1000
+    h2 = .5
+    outprefix = 'sim_output_with_indir/'
+    nfam = 30 * 10 ** 3
+    n_random = 0
+    n_am = 22
+    save_par_gts = True
+    impute = True
+    unphased_impute = False
+    r_par = .5
+    v_indir = .5
+    r_dir_indir = .5
+    beta_vert = 0
+
+class NoIndirArgs :
+    n_causal = 1000
+    h2 = 0.5
+    outprefix = 'sim_output_no_indir/'
+    nfam = 30 * 10 ** 3
+    n_random = 0
+    n_am = 22
+    save_par_gts = True
+    impute = True
+    unphased_impute = False
+    r_par = 0.5
+    v_indir = 0
+    r_dir_indir = None
+    beta_vert = 0
+
+if __name__ == '__main__' :
+    ar = WithIndirArgs()
+
+    main(ar)
